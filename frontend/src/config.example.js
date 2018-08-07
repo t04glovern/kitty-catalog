cd1b186421f60ce59f584e67421b39ea7ff6e7e4
Nathan Glover <nathan@glovers.id.au>
2018-08-07 22:16:52 +0800

    Configuring environments

--- frontend/src/config.example.js
+++ frontend/src/config.example.js
@@ -1,17 +1,44 @@
-export default {
-    MAX_ATTACHMENT_SIZE: 5000000,
+const dev = {
     s3: {
-        REGION: "YOUR_S3_UPLOADS_BUCKET_REGION",
-        BUCKET: "YOUR_S3_UPLOADS_BUCKET_NAME"
+      REGION: "YOUR_DEV_S3_UPLOADS_BUCKET_REGION",
+      BUCKET: "YOUR_DEV_S3_UPLOADS_BUCKET_NAME"
     },
     apiGateway: {
-        REGION: "YOUR_API_GATEWAY_REGION",
-        URL: "YOUR_API_GATEWAY_URL"
+      REGION: "YOUR_DEV_API_GATEWAY_REGION",
+      URL: "YOUR_DEV_API_GATEWAY_URL"
     },
     cognito: {
-        REGION: "YOUR_COGNITO_REGION",
-        USER_POOL_ID: "YOUR_COGNITO_USER_POOL_ID",
-        APP_CLIENT_ID: "YOUR_COGNITO_APP_CLIENT_ID",
-        IDENTITY_POOL_ID: "YOUR_IDENTITY_POOL_ID"
+      REGION: "YOUR_DEV_COGNITO_REGION",
+      USER_POOL_ID: "YOUR_DEV_COGNITO_USER_POOL_ID",
+      APP_CLIENT_ID: "YOUR_DEV_COGNITO_APP_CLIENT_ID",
+      IDENTITY_POOL_ID: "YOUR_DEV_IDENTITY_POOL_ID"
     }
-};
+  };
+
+  const prod = {
+    s3: {
+      REGION: "YOUR_PROD_S3_UPLOADS_BUCKET_REGION",
+      BUCKET: "YOUR_PROD_S3_UPLOADS_BUCKET_NAME"
+    },
+    apiGateway: {
+      REGION: "YOUR_PROD_API_GATEWAY_REGION",
+      URL: "YOUR_PROD_API_GATEWAY_URL"
+    },
+    cognito: {
+      REGION: "YOUR_PROD_COGNITO_REGION",
+      USER_POOL_ID: "YOUR_PROD_COGNITO_USER_POOL_ID",
+      APP_CLIENT_ID: "YOUR_PROD_COGNITO_APP_CLIENT_ID",
+      IDENTITY_POOL_ID: "YOUR_PROD_IDENTITY_POOL_ID"
+    }
+  };
+
+  // Default to dev if not set
+  const config = process.env.REACT_APP_STAGE === 'prod'
+    ? prod
+    : dev;
+
+  export default {
+    // Add common config values here
+    MAX_ATTACHMENT_SIZE: 5000000,
+    ...config
+  };
